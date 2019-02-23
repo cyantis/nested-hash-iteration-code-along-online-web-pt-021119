7027924e8d5e24acd709f57e20515f9c8bf04b1f
Cody Yantis <cyantis@gmail.com>
2019-02-23 00:11:31 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -2,6 +2,8 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-  binding.pry
+    contact_details_hash.each do |attribute, value|
+      binding.pry
+    end
   end
 end
